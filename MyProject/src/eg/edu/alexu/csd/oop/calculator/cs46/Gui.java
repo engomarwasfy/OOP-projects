13ed6646673fd6cc2bac2c2afb8fa7036c2e853f
wasfy <eng.omarwasfy@gmail.com>
2016-10-12 00:10:14 +0200

    6th try

--- MyProject/src/eg/edu/alexu/csd/oop/calculator/cs46/Gui.java
+++ MyProject/src/eg/edu/alexu/csd/oop/calculator/cs46/Gui.java
@@ -14,258 +14,258 @@ import java.awt.event.ActionEvent;
 
 public class Gui {
 
-	private MyCalcMethods m = new MyCalcMethods();
-	private JFrame frame;
-	private JTextField textFiled;
-	private JButton btnNext;
-	private JButton btnSave;
-	private JButton btnLoad;
-	private JButton btnEnter;
-	private String server;
-	private JLabel label;
+    private MyCalcMethods m = new MyCalcMethods();
+    private JFrame frame;
+    private JTextField textFiled;
+    private JButton btnNext;
+    private JButton btnSave;
+    private JButton btnLoad;
+    private JButton btnEnter;
+    private String server;
+    private JLabel label;
 
-	public Gui() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 646, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+    public Gui() {
+	frame = new JFrame();
+	frame.setBounds(100, 100, 646, 300);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.getContentPane().setLayout(null);
 
-		textFiled = new JTextField();
-		textFiled.setBounds(68, 11, 298, 20);
-		frame.getContentPane().add(textFiled);
-		textFiled.setColumns(10);
+	textFiled = new JTextField();
+	textFiled.setBounds(68, 11, 298, 20);
+	frame.getContentPane().add(textFiled);
+	textFiled.setColumns(10);
 
-		JButton prevButton = new JButton("prev");
-		prevButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if ((server = m.prev()) != null) {
-					textFiled.setText(server);
-				} else {
-					JOptionPane.showMessageDialog(null, "no prev");
-				}
-			}
-		});
-		prevButton.setBounds(502, 116, 89, 23);
-		frame.getContentPane().add(prevButton);
+	JButton prevButton = new JButton("prev");
+	prevButton.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent e) {
+		if ((server = m.prev()) != null) {
+		    textFiled.setText(server);
+		} else {
+		    JOptionPane.showMessageDialog(null, "no prev");
+		}
+	    }
+	});
+	prevButton.setBounds(502, 116, 89, 23);
+	frame.getContentPane().add(prevButton);
 
-		btnNext = new JButton("next");
-		btnNext.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if ((server = m.next()) != null) {
-					textFiled.setText(server);
-				} else {
-					JOptionPane.showMessageDialog(null, "no next");
-				}
-			}
-		});
-		btnNext.setBounds(502, 159, 89, 23);
-		frame.getContentPane().add(btnNext);
+	btnNext = new JButton("next");
+	btnNext.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent e) {
+		if ((server = m.next()) != null) {
+		    textFiled.setText(server);
+		} else {
+		    JOptionPane.showMessageDialog(null, "no next");
+		}
+	    }
+	});
+	btnNext.setBounds(502, 159, 89, 23);
+	frame.getContentPane().add(btnNext);
 
-		btnSave = new JButton("save");
-		btnSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				m.save();
-			}
-		});
-		btnSave.setBounds(502, 45, 89, 23);
-		frame.getContentPane().add(btnSave);
+	btnSave = new JButton("save");
+	btnSave.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		m.save();
+	    }
+	});
+	btnSave.setBounds(502, 45, 89, 23);
+	frame.getContentPane().add(btnSave);
 
-		btnLoad = new JButton("load");
-		btnLoad.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				m.load();
-				textFiled.setText(m.current());
-			}
-		});
-		btnLoad.setBounds(502, 79, 89, 23);
-		frame.getContentPane().add(btnLoad);
+	btnLoad = new JButton("load");
+	btnLoad.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent e) {
+		m.load();
+		textFiled.setText(m.current());
+	    }
+	});
+	btnLoad.setBounds(502, 79, 89, 23);
+	frame.getContentPane().add(btnLoad);
 
-		btnEnter = new JButton("=");
-		btnEnter.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+	btnEnter = new JButton("=");
+	btnEnter.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent e) {
 
-				try {
-					m.input(textFiled.getText());
-					label.setText(m.getResult());
-				} catch (Exception e1) {
-					JOptionPane.showMessageDialog(null, "wrong expression");
-				}
-			}
-		});
-		btnEnter.setBounds(321, 213, 45, 23);
-		frame.getContentPane().add(btnEnter);
+		try {
+		    m.input(textFiled.getText());
+		    label.setText(m.getResult());
+		} catch (Exception e1) {
+		    JOptionPane.showMessageDialog(null, "wrong expression");
+		}
+	    }
+	});
+	btnEnter.setBounds(321, 213, 45, 23);
+	frame.getContentPane().add(btnEnter);
 
-		label = new JLabel("");
-		label.setBounds(33, 230, 284, 14);
-		frame.getContentPane().add(label);
+	label = new JLabel("");
+	label.setBounds(33, 230, 284, 14);
+	frame.getContentPane().add(label);
 
-		JButton plus = new JButton("+");
-		plus.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "+");
-			}
-		});
-		plus.setBounds(321, 178, 45, 23);
-		frame.getContentPane().add(plus);
+	JButton plus = new JButton("+");
+	plus.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "+");
+	    }
+	});
+	plus.setBounds(321, 178, 45, 23);
+	frame.getContentPane().add(plus);
 
-		JButton minus = new JButton("-");
-		minus.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "-");
-			}
-		});
-		minus.setBounds(321, 143, 45, 23);
-		frame.getContentPane().add(minus);
+	JButton minus = new JButton("-");
+	minus.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "-");
+	    }
+	});
+	minus.setBounds(321, 143, 45, 23);
+	frame.getContentPane().add(minus);
 
-		JButton multiply = new JButton("*");
-		multiply.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "*");
-			}
-		});
-		multiply.setBounds(321, 102, 45, 23);
-		frame.getContentPane().add(multiply);
+	JButton multiply = new JButton("*");
+	multiply.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "*");
+	    }
+	});
+	multiply.setBounds(321, 102, 45, 23);
+	frame.getContentPane().add(multiply);
 
-		JButton divide = new JButton("/");
-		divide.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "/");
-			}
-		});
-		divide.setBounds(321, 65, 45, 23);
-		frame.getContentPane().add(divide);
+	JButton divide = new JButton("/");
+	divide.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "/");
+	    }
+	});
+	divide.setBounds(321, 65, 45, 23);
+	frame.getContentPane().add(divide);
 
-		JButton button7 = new JButton("7");
-		button7.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "7");
-			}
-		});
-		button7.setBounds(100, 65, 45, 23);
-		frame.getContentPane().add(button7);
+	JButton button7 = new JButton("7");
+	button7.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "7");
+	    }
+	});
+	button7.setBounds(100, 65, 45, 23);
+	frame.getContentPane().add(button7);
 
-		JButton button8 = new JButton("8");
-		button8.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "8");
-			}
-		});
-		button8.setBounds(171, 65, 45, 23);
-		frame.getContentPane().add(button8);
+	JButton button8 = new JButton("8");
+	button8.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "8");
+	    }
+	});
+	button8.setBounds(171, 65, 45, 23);
+	frame.getContentPane().add(button8);
 
-		JButton button9 = new JButton("9");
-		button9.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "9");
-			}
-		});
-		button9.setBounds(237, 65, 45, 23);
-		frame.getContentPane().add(button9);
+	JButton button9 = new JButton("9");
+	button9.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "9");
+	    }
+	});
+	button9.setBounds(237, 65, 45, 23);
+	frame.getContentPane().add(button9);
 
-		JButton button4 = new JButton("4");
-		button4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "4");
-			}
-		});
-		button4.setBounds(100, 102, 45, 23);
-		frame.getContentPane().add(button4);
+	JButton button4 = new JButton("4");
+	button4.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "4");
+	    }
+	});
+	button4.setBounds(100, 102, 45, 23);
+	frame.getContentPane().add(button4);
 
-		JButton button5 = new JButton("5");
-		button5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "5");
-			}
-		});
-		button5.setBounds(171, 102, 45, 23);
-		frame.getContentPane().add(button5);
+	JButton button5 = new JButton("5");
+	button5.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "5");
+	    }
+	});
+	button5.setBounds(171, 102, 45, 23);
+	frame.getContentPane().add(button5);
 
-		JButton button6 = new JButton("6");
-		button6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "6");
-			}
-		});
-		button6.setBounds(237, 102, 45, 23);
-		frame.getContentPane().add(button6);
+	JButton button6 = new JButton("6");
+	button6.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "6");
+	    }
+	});
+	button6.setBounds(237, 102, 45, 23);
+	frame.getContentPane().add(button6);
 
-		JButton button1 = new JButton("1");
-		button1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "1");
-			}
-		});
-		button1.setBounds(100, 143, 45, 23);
-		frame.getContentPane().add(button1);
+	JButton button1 = new JButton("1");
+	button1.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "1");
+	    }
+	});
+	button1.setBounds(100, 143, 45, 23);
+	frame.getContentPane().add(button1);
 
-		JButton button2 = new JButton("2");
-		button2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "2");
-			}
-		});
-		button2.setBounds(171, 143, 45, 23);
-		frame.getContentPane().add(button2);
+	JButton button2 = new JButton("2");
+	button2.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "2");
+	    }
+	});
+	button2.setBounds(171, 143, 45, 23);
+	frame.getContentPane().add(button2);
 
-		JButton button3 = new JButton("3");
-		button3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "3");
-			}
-		});
-		button3.setBounds(237, 143, 45, 23);
-		frame.getContentPane().add(button3);
+	JButton button3 = new JButton("3");
+	button3.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "3");
+	    }
+	});
+	button3.setBounds(237, 143, 45, 23);
+	frame.getContentPane().add(button3);
 
-		JButton button0 = new JButton("0");
-		button0.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "0");
-			}
-		});
-		button0.setBounds(100, 178, 89, 23);
-		frame.getContentPane().add(button0);
+	JButton button0 = new JButton("0");
+	button0.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "0");
+	    }
+	});
+	button0.setBounds(100, 178, 89, 23);
+	frame.getContentPane().add(button0);
 
-		JButton buttondot = new JButton(".");
-		buttondot.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + ".");
-			}
-		});
-		buttondot.setBounds(199, 178, 45, 23);
-		frame.getContentPane().add(buttondot);
+	JButton buttondot = new JButton(".");
+	buttondot.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + ".");
+	    }
+	});
+	buttondot.setBounds(199, 178, 45, 23);
+	frame.getContentPane().add(buttondot);
 
-		JButton buttonrt = new JButton("(");
-		buttonrt.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + "(");
-			}
-		});
-		buttonrt.setBounds(247, 178, 45, 23);
-		frame.getContentPane().add(buttonrt);
+	JButton buttonrt = new JButton("(");
+	buttonrt.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + "(");
+	    }
+	});
+	buttonrt.setBounds(247, 178, 45, 23);
+	frame.getContentPane().add(buttonrt);
 
-		JButton buttonlft = new JButton(")");
-		buttonlft.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText(textFiled.getText() + ")");
-			}
-		});
-		buttonlft.setBounds(237, 213, 45, 23);
-		frame.getContentPane().add(buttonlft);
+	JButton buttonlft = new JButton(")");
+	buttonlft.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText(textFiled.getText() + ")");
+	    }
+	});
+	buttonlft.setBounds(237, 213, 45, 23);
+	frame.getContentPane().add(buttonlft);
 
-		JButton buttonclear = new JButton("clear");
-		buttonclear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				textFiled.setText("");
-			}
-		});
-		buttonclear.setBounds(502, 201, 89, 23);
-		frame.getContentPane().add(buttonclear);
-		frame.setVisible(true);
-	}
+	JButton buttonclear = new JButton("clear");
+	buttonclear.addActionListener(new ActionListener() {
+	    public void actionPerformed(final ActionEvent arg0) {
+		textFiled.setText("");
+	    }
+	});
+	buttonclear.setBounds(502, 201, 89, 23);
+	frame.getContentPane().add(buttonclear);
+	frame.setVisible(true);
+    }
 
-	public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-		Gui gui = new Gui();
+	Gui gui = new Gui();
 
-	}
+    }
 }
--- MyProject/src/eg/edu/alexu/csd/oop/calculator/cs46/MyCalcMethods.java
+++ MyProject/src/eg/edu/alexu/csd/oop/calculator/cs46/MyCalcMethods.java
@@ -1,6 +1,5 @@
 package eg.edu.alexu.csd.oop.calculator.cs46;
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -16,157 +15,150 @@ import javax.script.ScriptEngineManager;
 
 import eg.edu.alexu.csd.oop.calculator.Calculator;
 
-
-
 public class MyCalcMethods implements Calculator {
-	private File fout;
-	private String s=null;;
-	private String result;
-	private String server;
-	private LinkedList<String>ls=new LinkedList<String>();
-	private int count = -1;
-	private int pointer = -1 ;
-	private Integer countint;
-	private Integer pointerint;
+    private File fout;
+    private String s = null;;
+    private String result;
+    private String server;
+    private LinkedList<String> ls = new LinkedList<String>();
+    private int count = -1;
+    private int pointer = -1;
+    private Integer countint;
+    private Integer pointerint;
 
-	@Override
-	public void input(String s) {
-		this.s = s;
-		if(getResult()!=null||s=="null"){
-		 if(count==4){
-		    	ls.removeFirst();
-		    	ls.addLast(s);
-		    	pointer=count;
-		    }
-		    else{
-		    	ls.addLast(s);
-		    	count++;
-		    	pointer=count;
-		    }
-		}
-		
+    @Override
+    public void input(String s) {
+	this.s = s;
+	if (getResult() != null || s == "null") {
+	    if (count == 4) {
+		ls.removeFirst();
+		ls.addLast(s);
+		pointer = count;
+	    } else {
+		ls.addLast(s);
+		count++;
+		pointer = count;
+	    }
 	}
 
-	@Override
-	public String getResult() {
-		
+    }
 
-		ScriptEngineManager mgr = new ScriptEngineManager();
-	    ScriptEngine engine = mgr.getEngineByName("JavaScript");
-	    try{
-	   result = String.valueOf(engine.eval(current()+"+0.0"));
-	    }
-	    catch(Exception e){
-	    	throw new RuntimeException();
-	    }
+    @Override
+    public String getResult() {
+
+	ScriptEngineManager mgr = new ScriptEngineManager();
+	ScriptEngine engine = mgr.getEngineByName("JavaScript");
+	try {
+	    result = String.valueOf(engine.eval(current() + "+0.0"));
+	} catch (Exception e) {
+	    throw new RuntimeException();
+	}
+
+	return result;
+    }
+
+    @Override
+    public String current() {
+	// TODO Auto-generated method stub
+	// will return null as initialized before if no input
+	if (pointer == -1) {
+	    return null;
+	}
+	return ls.get(pointer);
+    }
+
+    @Override
+    public String prev() {
+	// TODO Auto-generated method stub
+	if (pointer == 0 || pointer == -1) {
+	    return null;
+	}
+	pointer--;
+	try {
+	    server = ls.get(pointer);
+	} catch (Exception e) {
+	    // TODO: handle exception
+	    return null;
+	}
+	return server;
+    }
+
+    @Override
+    public String next() {
+	// TODO Auto-generated method stub
+	if (pointer == ls.size() - 1 || pointer == -1) {
+	    return null;
+	}
+	pointer++;
+	try {
+	    server = ls.get(pointer);
+	} catch (Exception e) {
 	   
-		return result;
+	    return null;
+	}
+	return server;
+    }
+
+    @Override
+    public void save() {
+	// TODO Auto-generated method stub
+	try {
+	    countint = new Integer(count);
+	    pointerint = new Integer(pointer);
+	    writeFile(ls);
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	@Override
-	public String current() {
-		// TODO Auto-generated method stub
-		//will return null as initialized before if no input
-		if(pointer==-1){
-			return null;
-		}
-		return ls.get(pointer);
+    }
+
+    @Override
+    public void load() {
+	// TODO Auto-generated method stub
+	try {
+	    fout = new File("out.txt");
+	    ls = readFile(fout);
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	@Override
-	public String prev() {
-		// TODO Auto-generated method stub
-		if (pointer==0||pointer==-1){
-			return null;
-		}
-		pointer--;
-		try {
-			server = ls.get(pointer);
-		} catch (Exception e) {
-			// TODO: handle exception
-			return null;
-		}
-		return server;
+    }
+
+    public void writeFile(LinkedList<String> s) throws IOException {
+
+	fout = new File("out.txt");
+	FileOutputStream fos = new FileOutputStream(fout, false);
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+	bw.append(countint.toString());
+	bw.newLine();
+	bw.append(pointerint.toString());
+	bw.newLine();
+
+	for (int i = 0; i < s.size(); i++) {
+	    bw.append(s.get(i));
+	    bw.newLine();
+	}
+	bw.close();
+    }
+
+    public LinkedList<String> readFile(File fin) throws IOException {
+	LinkedList<String> ls = new LinkedList<String>();
+	FileInputStream fis = new FileInputStream(fin);
+
+	// Construct BufferedReader from InputStreamReader
+	BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+	count = Integer.parseInt(br.readLine());
+	pointer = Integer.parseInt(br.readLine());
+
+	String line = null;
+	while ((line = br.readLine()) != null) {
+	    ls.addLast(line);
 	}
 
-	@Override
-	public String next() {
-		// TODO Auto-generated method stub
-		if(pointer==ls.size()-1||pointer==-1){
-			return null;
-		}
-		pointer++;
-		try {
-			server = ls.get(pointer);
-		} catch (Exception e) {
-			// TODO: handle exception
-			return null;
-		}
-		return server;
-	}
-
-	@Override
-	public void save() {
-		// TODO Auto-generated method stub
-		try {
-			countint = new Integer(count);
-			pointerint = new Integer(pointer);
-			writeFile(ls);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-
-	@Override
-	public void load() {
-		// TODO Auto-generated method stub
-		try {
-			fout = new File("out.txt");
-			ls=readFile(fout);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	public  void writeFile(LinkedList<String> s) throws IOException {
-		
-		  fout = new File("out.txt");
-		FileOutputStream fos = new FileOutputStream(fout,false);
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-		bw.append(countint.toString());
-		bw.newLine();
-		bw.append(pointerint.toString());
-		bw.newLine();
-		
-		for(int i=0;i<s.size();i++){
-			bw.append(s.get(i));
-			bw.newLine();
-		}
-		bw.close();
-	}
-	
-	
-	public  LinkedList<String> readFile(File fin) throws IOException {
-		LinkedList<String>ls = new LinkedList<String>();
-		FileInputStream fis = new FileInputStream(fin);
-	
-		//Construct BufferedReader from InputStreamReader
-		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-		count = Integer.parseInt(br.readLine());
-		pointer = Integer.parseInt(br.readLine());
-	 
-		String line = null;
-		while ((line = br.readLine()) != null) {
-			ls.addLast(line);
-		}
-	 
-		br.close();
+	br.close();
 	return ls;
+    }
+
 }
-
-
-}
-
