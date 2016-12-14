0b5e5cadc0d63dc7d9f40de349f7d95924742571
wasfy <eng.omarwasfy@gmail.com>
2016-12-14 07:57:44 +0200

    try

--- DataBaseNew/src/driver/MyDriver.java
+++ DataBaseNew/src/driver/MyDriver.java
@@ -1,76 +1,76 @@
 package driver;
 
-import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
+import java.util.Iterator;
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Logger;
 
-import JDBC.JConnection;
-
 public class MyDriver implements Driver {
-    
-    static {
-        try {
-            java.sql.DriverManager.registerDriver(new MyDriver());
-        } catch (SQLException E) {
-            throw new RuntimeException("Can't register driver!");
-        }
-    }
-    
-    public MyDriver(){
-	
-    }
-    @Override
-    public boolean acceptsURL(String arg0) throws SQLException {
-	if(arg0 == null) {
-	    throw new RuntimeException();
+
+	@Override
+	public boolean acceptsURL(String url) throws SQLException {
+		String[]arr = url.split(":");
+		if(arr.length==3&&arr[0].equals("jdpc")&&arr[1].equals("sql")){
+			if(Files.exists(Paths.get(arr[2]))){
+				return true;
+			}
+		}
+	 
+		
+		return false;
 	}
-	String fixedxml = "jdbc:xmldb://localhost" ;
-	String fixedjson = "jdbc:jsondb://localhost" ;
-	if((arg0.equalsIgnoreCase(fixedxml)) || (arg0.equalsIgnoreCase(fixedjson))) {
-	    return true;
+
+	@Override
+	public Connection connect(String url, Properties info) throws SQLException {
+		// TODO Auto-generated method stub
+		String userName =info.getProperty("username");
+		String password =info.getProperty("password");
+		//check user name and password
+		return DriverManager.getConnection(url, info);
 	}
-	return false;
-    }
 
-    @Override
-    public Connection connect(String arg0, Properties arg1) throws SQLException {
-	String tmp = System.getProperty("java.io.tmpdir");
-	final File DBMSdir = new File(tmp + "DBMS");
-	File[] list = DBMSdir.listFiles();
-	if(acceptsURL(arg0)) {
-	    JConnection.getinstance().setProtocol(arg0.split(":")[1]);   
+	@Override
+	public int getMajorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	return JConnection.getinstance();
-    }
 
-    @Override
-    public int getMajorVersion() {
-	return 0;
-    }
+	@Override
+	public int getMinorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    @Override
-    public int getMinorVersion() {
-	return 0;
-    }
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
 
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-	return null;
-    }
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+		Set<Object>set = info.keySet();
+		Iterator<Object>it = set.iterator();
+		DriverPropertyInfo[]driverPropertyInfos = new DriverPropertyInfo[info.size()];
+		for (int i = 0; i < driverPropertyInfos.length; i++) {
+			driverPropertyInfos[i] = new DriverPropertyInfo(it.next().toString(),info.getProperty(it.toString()));
+		}
+		return null;
+	}
 
-    @Override
-    public DriverPropertyInfo[] getPropertyInfo(String arg0, Properties arg1) throws SQLException {
-	return null;
-    }
-
-    @Override
-    public boolean jdbcCompliant() {
-	return false;
-    }
+	@Override
+	public boolean jdbcCompliant() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
