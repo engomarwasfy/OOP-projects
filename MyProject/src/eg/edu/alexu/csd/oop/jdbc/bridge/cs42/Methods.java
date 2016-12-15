289e8bce4dd4cf0300849609016661da3f205885
wasfy <eng.omarwasfy@gmail.com>
2016-12-15 14:04:28 +0200

    try

--- MyProject/src/eg/edu/alexu/csd/oop/jdbc/bridge/cs42/Methods.java
+++
@@ -1,9 +0,0 @@
-package eg.edu.alexu.csd.oop.jdbc.bridge.cs42;
-
-import java.io.File;
-
-public class Methods {
-    public static File getXmlFile(String dataBase, String table) {
-	return new File(dataBase + "\\" + table + ".xml");
-    }
-}
